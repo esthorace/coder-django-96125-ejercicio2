0cb6af7865201b954eda1701a1f6046aa70c764a
Esteban <esteban.horacio.acevedo.gmail.com>
2026-04-09 19:22:06 -0300

    Crea modelo Cliente

--- src/servicios/admin.py
+++ src/servicios/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from servicios import models
+
+
+@admin.register(models.Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ("apellido", "nombre", "celular")
+    search_fields = ("apellido", "nombre", "celular")
+    ordering = ("apellido", "nombre")
--- src/servicios/models.py
+++ src/servicios/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    celular = PhoneNumberField()
+    domicilio = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"{self.apellido}, {self.nombre}"
