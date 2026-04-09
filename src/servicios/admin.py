a2c928ea4a40d32bcbf173f12c91c2db2c51b566
Esteban <esteban.horacio.acevedo.gmail.com>
2026-04-09 19:47:02 -0300

    Crea modelo Servicio

--- src/servicios/admin.py
+++ src/servicios/admin.py
@@ -8,3 +8,11 @@ class ClienteAdmin(admin.ModelAdmin):
     list_display = ("apellido", "nombre", "celular")
     search_fields = ("apellido", "nombre", "celular")
     ordering = ("apellido", "nombre")
+
+
+@admin.register(models.Servicio)
+class ServicioAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "precio", "disponible")
+    list_filter = ("disponible",)
+    search_fields = ("nombre",)
+    ordering = ("nombre",)
--- src/servicios/models.py
+++ src/servicios/models.py
@@ -3,10 +3,20 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Cliente(models.Model):
-    nombre = models.CharField(max_length=100)
-    apellido = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100, db_index=True)
+    apellido = models.CharField(max_length=100, db_index=True)
     celular = PhoneNumberField()
     domicilio = models.CharField(max_length=255)
 
     def __str__(self) -> str:
         return f"{self.apellido}, {self.nombre}"
+
+
+class Servicio(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    descripcion = models.TextField(blank=True, null=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    disponible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
